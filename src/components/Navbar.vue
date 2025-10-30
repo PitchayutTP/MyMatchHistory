61108d41cfee740d4abe6f61272c1d84d753a35d
PitchayutTP <66070133@kmitl.ac.th>
2025-10-31 02:49:45 +0700

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,7 +20,7 @@
             <div class="relative">
                 <button @click="toggleDropdown"
                     class="flex items-center text-gray-700 hover:text-orange-600 focus:outline-none">
-                    <span class="font-medium">{{ username }}</span>
+                    <span class="font-medium">{{ firstname }}</span>
                     <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
